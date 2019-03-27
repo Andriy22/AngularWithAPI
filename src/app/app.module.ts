bc8d02cfa63290991a40605071cdeb09afd2ec82
Andriy22 <istep.andry@gmail.com>
2019-03-27 16:27:13 +0200

    added modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Input } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { WorkersComponent } from './workers/workers.component';
 
+import {Workers} from './workers';
+import {WorkersService} from './workers.service';
+
+import {FormsModule, ReactiveFormsModule} from '@angular/forms'
+import {HttpClientModule, HttpClient} from '@angular/common/http'
+
+import {MatButtonModule, MatMenuModule, MatCheckboxModule} from '@angular/material';
+import {MatNativeDateModule, MatTableModule, MatInputModule} from '@angular/material';
+import {MatTooltipModule, MatCardModule, MatIconModule, MatFormFieldModule} from '@angular/material';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WorkersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatCheckboxModule,
+    MatNativeDateModule,
+    MatTableModule,
+    MatInputModule,
+    MatTooltipModule,
+    MatCardModule,
+    MatIconModule,
+    MatFormFieldModule
   ],
-  providers: [],
+  providers: [WorkersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
